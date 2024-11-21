7d74030e8332b4fac712e19e9224a792de8cdee0
beomjo <dongjun1315@naver.com>
2024-11-21 17:13:38 +0900

    feat : 초기 라우터 구현

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Router from '../src/components/router';
+
+
+function App() {
+  return (
+    <Router />
+  );
+}
+
+export default App;
---
+++ src/components/mainPage.jsx
@@ -0,0 +1,9 @@
+function MainPage() {
+    return (
+        <div>
+            <h2>test2</h2>
+        </div>
+    )
+}
+
+export default MainPage;
